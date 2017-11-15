a0ca01956f07753bc67e80eea5a970f2e6e117ee
Calvin Kwong <ckwong93@gmail.com>
2017-11-15 15:30:07 -0800

    finish lettersort using hash table

--- Misc/letterSort.js
+++ Misc/letterSort.js
@@ -14,3 +14,31 @@
 // hello --> ehllo
 // whiteboard --> abdehiortw
 // one --> eno
+
+function letterSort(string){
+  var alphabet = 'abcdefghijklmnopqrstuvwxyz';
+  var alphabetArray = alphabet.split('');
+  var letters = string.split('');
+  var collection = {};
+  var finished = "";
+
+  for(var i = 0; i < alphabetArray.length; i++){
+    collection[alphabetArray[i]] = 0;
+  }
+
+  // console.log(collection);
+  for(var j = 0; j < letters.length; j++){
+    collection[letters[j]]++;
+  }
+
+  for(var items in collection){
+    if(collection[items] > 0){
+      finished += items.repeat(collection[items]);
+    }
+  }
+  return finished;
+}
+
+console.log(letterSort('hello'));
+console.log(letterSort('whiteboard'));
+console.log(letterSort('eno'));
