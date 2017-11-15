6e6d994c2d3006c8e7b337f9c91d742c03998b78
Calvin Kwong <ckwong93@gmail.com>
2017-11-15 15:56:28 -0800

    finishe character mode using hash counter

--- Misc/characterMode.js
+++ Misc/characterMode.js
@@ -15,3 +15,35 @@
 // 'A walk in the park' --> 'a'
 // 'noon' --> 'no'
 //
+function characterMode(string){
+  var letterCount = {};
+  var letters = string.toLowerCase().split('');
+  var modeNum = 0;
+  var mode = "";
+  // console.log(letters);
+  for(var i = 0; i < letters.length; i++){
+    if(letterCount[letters[i]] && letters[i] !== ' '){
+      letterCount[letters[i]]++;
+    }
+    else{
+      letterCount[letters[i]] = 1;
+    }
+  }
+  // console.log(letterCount);
+  for(var item in letterCount){
+    if(letterCount[item] > modeNum){
+      modeNum = letterCount[item];
+    }
+  }
+
+  for(var item2 in letterCount){
+    if(letterCount[item2] >= modeNum){
+      mode += item2;
+    }
+  }
+  return mode;
+}
+
+console.log(characterMode('hello'));
+console.log(characterMode('A walk in the park'));
+console.log(characterMode('noon'));
