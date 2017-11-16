6c4bb2e8faf9c6eeb613e0f75610d543585761b2
Calvin Kwong <ckwong93@gmail.com>
2017-11-16 11:50:18 -0800

    create algorithm to count 1s in sorted array using decrease and conquer method

---
+++ Misc/numberOfOnes.js
@@ -0,0 +1,40 @@
+// 1. Number of Ones
+// Utilize the decrease and conquer pattern to solve these problems.
+// Given a sorted bit array (values of either 0 or 1), determine the number of 1's in the array.
+//
+// Parameters
+// Input: arr {Array of Integers}
+// Output: {Integer}
+//
+// Constraints
+// Time: O(logN)
+// Space: O(1)
+//
+// Examples
+// [0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1] --> 8
+// [0, 0, 0] --> 0
+// [0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1] --> 7
+
+function oneCounter(numbers){
+  var end = numbers.length - 1;
+  var start = 0;
+  var mid;
+  var count = 0;
+
+  while(start <= end){
+    mid = Math.floor((start+end)/2);
+    if(numbers[mid] === 1){
+      end = mid -1;
+    }
+    else if(numbers[mid] === 0){
+      start = mid + 1;
+    }
+  }
+  count = numbers.length - start;
+  return count;
+}
+
+
+console.log(oneCounter([0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1]));
+console.log(oneCounter([0, 0, 0]));
+console.log(oneCounter([0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1]));
