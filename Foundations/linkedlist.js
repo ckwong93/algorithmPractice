e9ca91d06d2c1e612787ee69d15380c4a24d383b
Calvin Kwong <ckwong93@gmail.com>
2017-12-31 11:43:34 -0800

    complete printbackwards using recursion

--- Foundations/linkedlist.js
+++ Foundations/linkedlist.js
@@ -215,6 +215,30 @@ function printForward(node) {
   }
 }
 
+/**
+ * 1b. Print the value of each node backwards from the tail to the input node
+ * using recursion.
+ *
+ * Input: node {ListNode}
+ * Output: {undefined}
+ *
+ * Example: (1) --> (5) --> (7) --> (10)
+ *
+ *          10
+ *          7
+ *          5
+ *          1
+ */
+
+ // Time Complexity: O(n)
+ // Auxiliary Space Complexity: O(1)
+ function printBackward(node) {
+    if(node === null) {return}
+     printBackward(node.next);
+     console.log(node.value);
+ }
+
+
 ////////////////////////////////////////////////////////////
 ///////////////  DO NOT TOUCH TEST BELOW!!!  ///////////////
 ////////////////////////////////////////////////////////////
