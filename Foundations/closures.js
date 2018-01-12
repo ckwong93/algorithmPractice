237dae0028a792f8aeb98bdb3b0584c68ab1046d
Calvin Kwong <ckwong93@gmail.com>
2018-01-11 22:13:58 -0800

    add settimeout closure example

---
+++ Foundations/closures.js
@@ -0,0 +1,21 @@
+// closure - when a function within a function is called, it has access to its lexical scope - aka context variables needed for the inner function
+
+
+var multiplier = function(base){
+  var factor = function(number){
+    return base *= number
+  }
+  return factor;
+}
+
+let five = multiplier(5);
+five(3)
+five(7)
+
+
+
+// prints all 6's because i is hoisted to the top. if we add a closure, it will log correctly
+// output -> 55555
+for (var i = 1; i <= 5; i++) {
+    setTimeout(function() { console.log(i); }, 1000+i);
+}
