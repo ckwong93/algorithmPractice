6c4fc6583c1de938a14550269b75ae9922adc29f
calvin <ckwong93@gmail.com>
2018-01-19 13:30:20 -0800

    solve div sum pairs

--- HackerRank/divisibleSumPairs.js
+++ HackerRank/divisibleSumPairs.js
@@ -21,3 +21,17 @@
 // Explanation
 //
 // Here are the  valid pairs:
+function divisibleSumPairs(n, k, ar) {
+    // Complete this function
+  let count = 0;
+  for(var i = 0; i < ar.length - 1; i++){
+    for(var j = i+1; j < ar.length; j++){
+      let currSum = ar[i] + ar[j];
+      if(i < j && currSum % k == 0){
+        console.log(i,j);
+        count++;
+      }
+    }
+  }
+  return count;
+}
