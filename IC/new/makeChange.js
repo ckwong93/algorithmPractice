1893977c733dce7ba54ac7c1daae5d4c664a3ef0
calvin <ckwong93@gmail.com>
2018-01-22 14:10:24 -0800

    setup make change

---
+++ IC/new/makeChange.js
@@ -0,0 +1,16 @@
+// Your quirky boss collects rare, old coins...
+
+// They found out you're a programmer and asked you to solve something they've been wondering for a long time.
+
+// Write a function that, given:
+
+// an amount of money
+// an array of coin denominations
+// computes the number of ways to make the amount of money with coins of the available denominations.
+
+//     Example: for amount = 444444(444444¢) and denominations = [1, 2, 3][1, 2, 3][1, 2, 3][1, 2, 3][1, 2, 3][1, 2, 3](111111¢, 222222¢ and 333333¢), your program would output 444444—the number of ways to make 444444¢ with those denominations:
+
+//     1¢, 1¢, 1¢, 1¢
+// 1¢, 1¢, 2¢
+// 1¢, 3¢
+// 2¢, 2¢
