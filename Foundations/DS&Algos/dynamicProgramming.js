608d91fd73d415cecc9ecd33dc3ca23fa6b52735
calvin <ckwong93@gmail.com>
2018-01-23 17:05:25 -0800

    init dynamic programming - write definition

---
+++ Foundations/DS&Algos/dynamicProgramming.js
@@ -0,0 +1,5 @@
+// dynamic programming allows us to shave off time complexity by keeping data stored. as a result, we will not need to recalculate recurring items.
+// there are two types of dynamic programming - memoization and tabulation. 
+
+// memoization is often used with recursion - storing saved calls into a hashmap lets you retrieve them quickly and thus speed up the time complexity by a wide margin
+
