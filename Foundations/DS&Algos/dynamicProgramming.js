2db3eea80e229557b267e0df775be3b72bd36f02
calvin <ckwong93@gmail.com>
2018-01-23 17:10:00 -0800

    solve fibo with DP and non DP

--- Foundations/DS&Algos/dynamicProgramming.js
+++ Foundations/DS&Algos/dynamicProgramming.js
@@ -3,3 +3,40 @@
 
 // memoization is often used with recursion - storing saved calls into a hashmap lets you retrieve them quickly and thus speed up the time complexity by a wide margin
 
+// ex 1: Dynamic Programming (memoization) with the Fibonacci Sequence
+
+// non-DP solution
+function fibonacci(n){
+    if(n < 2){
+        return n;
+    }
+    else{
+        return fibonacci(n-1) + fibonacci(n-2);
+    }
+}
+
+// fibonacci(5) -> 5
+// fibonacci(10) -> 55
+// console.log(fibonacci(5));
+// console.log(fibonacci(10));
+
+// DP solution using memoization
+function fibonacci(n){
+    let memo = {};
+    function fib(n){
+        if(memo[n]){
+            return memo[n]
+        }
+        else if(n < 2){
+            return n;
+        }
+        else{
+            memo[n] = fib(n-1) + fib(n-2);
+        }
+        return memo[n]
+    }
+    return fib(n)
+}
+
+console.log(fibonacci(5));
+console.log(fibonacci(10));
