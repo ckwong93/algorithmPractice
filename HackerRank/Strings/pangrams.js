c8d8706fa3edce734203d5f19f6bddb18412f72a
calvin <ckwong93@gmail.com>
2018-01-29 20:42:44 -0800

    solve pangram

--- HackerRank/Strings/pangrams.js
+++ HackerRank/Strings/pangrams.js
@@ -35,3 +35,23 @@
 
 // In the first test case, the answer is pangram because the sentence contains all the letters of the English alphabet.
 
+function pangram(string) {
+    let letters = { a: 0, b: 0, c: 0, d: 0, e: 0, f: 0, g: 0, h: 0, i: 0, j: 0, k: 0, l: 0, m: 0, n: 0, o: 0, p: 0, q: 0, r: 0, s: 0, t: 0, u: 0, v: 0, w: 0, x: 0, y: 0, z: 0 };
+    lowercased = string.toLowerCase();
+    for (var i = 0; i < lowercased.length; i++) {
+        if (lowercased[i] !== ' ') {
+            letters[lowercased[i]]++;
+        }
+    }
+
+    for (var items in letters) {
+        if (letters[items] < 1) {
+            return 'not pangram'
+        }
+    }
+    return 'pangram'
+}
+
+console.log(pangram('We promptly judged antique ivory buckles for the next prize'));
+console.log(pangram('We promptly judged antique ivory buckles for the prize'));
+
