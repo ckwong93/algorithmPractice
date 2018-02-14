d38e5540daaac6614e4c0bbd6fc798bcc3f8d357
calvin <ckwong93@gmail.com>
2018-02-13 17:46:19 -0800

    add notes on Model

--- Notes/MVC.js
+++ Notes/MVC.js
@@ -2,6 +2,15 @@
 
 // Model-View-Controller: architectural paradigm. NOT a framework. structure of application will be built in a certain way (model, view, controller)
 
-// git Model
-// View
-// Controller
+// Model - Database
+// View - Client
+// Controller - Server
+
+// Code related to DB - Model;
+// code processing info to and from db - controller
+// output frontend code - view
+
+// Model
+// -adding and retreiving items from DB
+// -processing data from or to the database/
+// -speaks only with the controller. DOES NOT SPEAK TO VIEWS
