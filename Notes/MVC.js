acfcd9f6fb6fcc0e76ab8918d8f932dcd522c449
calvin <ckwong93@gmail.com>
2018-02-13 17:30:49 -0800

    init notes section, work on MVC

---
+++ Notes/MVC.js
@@ -0,0 +1,7 @@
+// MVC
+
+// Model-View-Controller: architectural paradigm. NOT a framework. structure of application will be built in a certain way (model, view, controller)
+
+// git Model
+// View
+// Controller
