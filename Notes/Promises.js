7fc8f1308b438a778cb55294a07a5e6dfb21553a
calvin <ckwong93@gmail.com>
2018-02-16 11:09:07 -0800

    add NetNinja jQuery example

--- Notes/Promises.js
+++ Notes/Promises.js
@@ -29,3 +29,17 @@ cleanRoom().then(function(message){
 }).then(function(message){
     console.log(message + ' - all complete!')
 })
+
+
+//NetNinja jQuery example
+$.get("data/tweets.json").then(function(tweets){
+    console.log(tweets);
+    return $.get("data/friends.json");
+}).then(function(friends){
+    console.log(friends);
+    return $.get("data/videos.json");
+}).then(function(videos){
+    console.log(videos);
+    console.log('all done!')
+})
+
