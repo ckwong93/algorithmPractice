3238389bf9cde14ebf686cdb9740c73417f468b2
calvin <ckwong93@gmail.com>
2018-02-17 11:11:22 -0800

    init generators file

---
+++ Notes/Generators.js
@@ -0,0 +1,4 @@
+// JavaScript Generator
+
+// A generator is essentially a pausable function using 'yield'. doing so lets you pause and resume the function whenever seems fitting. this is a very useful tool with asynchronous code
+
