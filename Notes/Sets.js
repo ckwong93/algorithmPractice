9720b70a01fd513527f61f99da6716aa80d41404
calvin <ckwong93@gmail.com>
2018-02-22 12:15:24 -0800

    add set notes for adding new values and explaining duplicate values

--- Notes/Sets.js
+++ Notes/Sets.js
@@ -1,1 +1,12 @@
 // JavaScript Sets - unique values of any type - similar to an array, but with UNIQUE values only
+
+// example:
+let test = new Set();
+test.add('first').add('second').add('third').add('fourth');
+console.log(test)
+// ouput = Set { 'first', 'second', 'third', 'fourth' }
+
+// adding a duplicate will not change the output
+test.add('first')
+console.log(test)
+// ouput = Set { 'first', 'second', 'third', 'fourth' }
