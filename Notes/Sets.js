4205f2eaa963f2f1862e61d553200e6023993b33
calvin <ckwong93@gmail.com>
2018-02-22 12:22:32 -0800

    add notes on checking if values exist in set using has

--- Notes/Sets.js
+++ Notes/Sets.js
@@ -17,6 +17,12 @@ console.log(test);
 // output = Set { 'second', 'third', 'fourth' }
 // ** chaining delete methods does not work - if you console.log(test.delete('first')), it will return a boolean (t/f)
 
+// checking if value exists in set
+console.log(test.has('first'), 'has first?');
+// output false because it gets deleted above
+console.log(test.has('second'), 'has second?')
+// output -true
+
 // converting an array with dupes to a set
 let testArray = ['a','b','c','d','e','f','f','e','d','c','b','a'];
 let testSet = new Set(testArray);
@@ -29,3 +35,4 @@ setItems.add('a').add('b').add('c').add('d');
 let convertedToArr = [...setItems];
 console.log(convertedToArr);
 // output ['a', 'b', 'c', 'd']
+
