738e3a2ce6b7d562d7c6e1d9187b246e8a7834db
calvin <ckwong93@gmail.com>
2018-02-22 12:17:13 -0800

    add notes for delete methods...chaining delete methods DOES NOT WORK

--- Notes/Sets.js
+++ Notes/Sets.js
@@ -10,3 +10,10 @@ console.log(test)
 test.add('first')
 console.log(test)
 // ouput = Set { 'first', 'second', 'third', 'fourth' }
+
+// removing values
+test.delete('first');
+console.log(test);
+// output = Set { 'second', 'third', 'fourth' }
+// ** chaining delete methods does not work - if you console.log(test.delete('first')), it will return a boolean (t/f)
+
