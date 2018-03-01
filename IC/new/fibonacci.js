fa7881694c9877d3bc6dbc5397b049b4a9fd53ca
calvin <ckwong93@gmail.com>
2018-02-28 16:57:50 -0800

    solve iteratively

--- IC/new/fibonacci.js
+++ IC/new/fibonacci.js
@@ -1,4 +1,5 @@
 // Write a function fib() that a takes an integer n and returns the nth fibonacci 
+
 // solve recursively
 function fibonacci(n) {
     let nums = [0, 1];
@@ -9,4 +10,17 @@ function fibonacci(n) {
     }
 }
 
-fibonacci(8)
+console.log(fibonacci(8))
+
+
+// solve iteratively
+function fibonacciIterative(n) {
+    let nums = [0, 1];
+    for (var i = 2; i <= n; i++) {
+        nums[i] = nums[i - 1] + nums[i - 2]
+    }
+    console.log(nums)
+    return nums[n]
+}
+
+console.log(fibonacciIterative(8));
