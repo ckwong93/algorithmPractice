377cae1fc726f74631dd8b1222567db3835fe4d5
calvin <ckwong93@gmail.com>
2018-02-28 16:57:23 -0800

    solve recursively

--- IC/new/fibonacci.js
+++ IC/new/fibonacci.js
@@ -1,1 +1,12 @@
 // Write a function fib() that a takes an integer n and returns the nth fibonacci 
+// solve recursively
+function fibonacci(n) {
+    let nums = [0, 1];
+    if (n <= 1) {
+        return nums[n]
+    } else {
+        return fibonacci(n - 1) + fibonacci(n - 2)
+    }
+}
+
+fibonacci(8)
