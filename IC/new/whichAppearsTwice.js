4db1da090fd6de4de03ffca629592f635652ac7c
calvin <ckwong93@gmail.com>
2018-03-03 14:50:37 -0800

    solve whichAppearsTwice

--- IC/new/whichAppearsTwice.js
+++ IC/new/whichAppearsTwice.js
@@ -3,6 +3,15 @@
 // Write a function for finding the number that appears twice.
 
 
+
 function whichAppearsTwice(numbers){
-    
+ let counter = {};
+ for(var i = 0; i < numbers.length; i++){
+     if(counter[numbers[i]]){
+         return numbers[i] 
+     } else{
+         counter[numbers[i]] = 1;
+     }
+ }
+ return 'no dupes found'   
 }
