d3619d60b55a3c3a85f2b432198661452ea0b0b1
calvin <ckwong93@gmail.com>
2018-03-05 20:28:29 -0800

    solve random between 3 & 11

--- Misc/randomBetween.js
+++ Misc/randomBetween.js
@@ -3,3 +3,8 @@
 function randomBetween(){
     return Math.floor(Math.random() * 3)+ 5
 }
+
+// generate a random number between 3 & 11
+function randomThreeEleven(){
+    return Math.floor(Math.random() * 9) + 3
+}
