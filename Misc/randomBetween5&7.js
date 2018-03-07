2ccfd96dfc45a98ac232d432b3d5dcc8dfb10af6
calvin <ckwong93@gmail.com>
2018-03-06 16:54:27 -0800

    init reverse words

--- Misc/randomBetween5&7.js
+++
@@ -1,6 +0,0 @@
-// generate a random number between 5 and 7
-
-function randomBetween(){
-    return Math.floor(Math.random() * 3)+ 5
-}
-
---
+++ Tech Challenges/reverseWords.js
@@ -0,0 +1,3 @@
+// given a string, write a function that will reverse the letters in a word
+// hello world -> olleh dlrow
+
