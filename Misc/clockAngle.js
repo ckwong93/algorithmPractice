18a7f882f56821a4a8d0042a3b0b77bac9ad3a61
calvin <ckwong93@gmail.com>
2018-03-07 16:11:42 -0800

    solve clockAngle

--- Misc/clockAngle.js
+++ Misc/clockAngle.js
@@ -1,1 +1,22 @@
 // given a hour and minute, calculate the clock angle that it creates
+
+
+function clockAngle(hr,min){
+    let degreesPerHour = 30;
+    let degreesPerMin = 6;
+    let hourAngle;
+    let minuteAngle;
+    let result;
+
+    minuteAngle = min * degreesPerMin;
+    hourAngle = (hr * degreesPerHour) + ((min / 60) * degreesPerHour);
+    result = Math.abs(minuteAngle - hourAngle);
+
+    if(result > 180){
+        return 360 - result;
+    }
+    return result
+
+}
+// console.log(clockAngle(2,30))
+console.log(clockAngle(4,42))
