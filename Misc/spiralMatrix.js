a6f4fb99801057feb325e6cf4cebc821b75e77ae
calvin <ckwong93@gmail.com>
2018-03-07 19:07:14 -0800

    add test cases

--- Misc/spiralMatrix.js
+++ Misc/spiralMatrix.js
@@ -45,3 +45,6 @@ function spiralizer(matrix) {
     return results;
 }
 
+console.log(spiralizer([[1, 2, 3], [4, 5, 6], [7, 8, 9]]))
+console.log(spiralizer([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]]))
+
